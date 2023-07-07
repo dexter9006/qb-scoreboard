9f8361b355a6749455becb54d11e2468935f5d1c
dexter9006 <52006237+dexter9006@users.noreply.github.com>
2023-07-07 23:19:59 +0100

    Update config.lua

--- config.lua
+++ config.lua
@@ -3,7 +3,7 @@ Config = Config or {}
 -- Open scoreboard key
 Config.OpenKey = 'HOME' -- https://docs.fivem.net/docs/game-references/input-mapper-parameter-ids/keyboard/
 
-Config.Toggle = true -- If this is false the scoreboard stays open only when you hold the OpenKey button, if this is true the scoreboard will be openned and closed with the OpenKey button
+Config.Toggle = false -- If this is false the scoreboard stays open only when you hold the OpenKey button, if this is true the scoreboard will be openned and closed with the OpenKey button
 
 -- Max Server Players
 Config.MaxPlayers = GetConvarInt('sv_maxclients', 48) -- It returns 48 if it cant find the Convar Int
@@ -38,4 +38,4 @@ Config.IllegalActions = {
 }
 
 -- Show ID's for all players or Opted in Staff
-Config.ShowIDforALL = false
+Config.ShowIDforALL = true
